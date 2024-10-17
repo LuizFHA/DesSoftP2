2f4e31fe3b8879707dacfb54057d90d035aaec98
LuizAssaf <felipe.h.assaf@gmail.com>
2024-10-17 13:27:50 -0300

    função preenche_frota

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,10 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
                 posição.append([linha, coluna+i])
                 i += 1
     return posição
+
+def preenche_frota(infofrota, nomenavio, linha, coluna, orientacao, tamanho):
+    if nomenavio not in infofrota:
+        infofrota[nomenavio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+    else:
+        infofrota[nomenavio].append(define_posicoes(linha, coluna, orientacao, tamanho))
+    return infofrota
