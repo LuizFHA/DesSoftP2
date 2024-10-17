b47068a6c0ffe8244fbb48f12e40f362ca00425c
LuizAssaf <felipe.h.assaf@gmail.com>
2024-10-17 15:02:49 -0300

    primeiro programa do jogo

---
+++ programa.py
@@ -0,0 +1,74 @@
+from funcoes import *
+frota = {
+    "porta-aviões":[],
+    "navio-tanque":[],
+    "contratorpedeiro":[],
+    "submarino": [],
+}     
+nome = 'porta-aviões'
+tamanho = 4
+quantidade = 1
+while quantidade > 0:
+    print('Insira as informações referentes ao navio',nome,'que possui tamanho',tamanho)
+    linha = input('Digite a linha ')
+    coluna = input('Digite a coluna ')
+    orientacao = input('Digite a orientacao ')
+    if orientacao == 1:
+        orientacao = "vertical"
+    elif orientacao == 2:
+        orientacao = "horizontal"
+    if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
+        print('Esta posição não está válida!')
+    else:
+        frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+        quantidade -= 1
+    
+nome = "navio-tanque"
+tamanho = 3
+quantidade = 2
+while quantidade > 0:
+    print('Insira as informações referentes ao navio',nome,'que possui tamanho',tamanho)
+    linha = input('Digite a linha ')
+    coluna = input('Digite a coluna ')
+    orientacao = input('Digite a orientacao ')
+    if orientacao == 1:
+        orientacao = "vertical"
+    elif orientacao == 2:
+        orientacao = "horizontal"
+    if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
+        print('Esta posição não está válida!')
+    else:
+        frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+        quantidade -= 1
+
+nome = "contratorpedeiro"
+tamanho = 2
+quantidade = 3
+while quantidade > 0:
+    print('Insira as informações referentes ao navio',nome,'que possui tamanho',tamanho)
+    linha = input('Digite a linha ')
+    coluna = input('Digite a coluna ')
+    orientacao = input('Digite a orientacao ')
+    if orientacao == 1:
+        orientacao = "vertical"
+    elif orientacao == 2:
+        orientacao = "horizontal"
+    if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
+        print('Esta posição não está válida!')
+    else:
+        frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+        quantidade -= 1
+
+nome = "submarino"
+tamanho = 1
+quantidade = 4
+while quantidade > 0:
+    print('Insira as informações referentes ao navio',nome,'que possui tamanho',tamanho)
+    linha = input('Digite a linha ')
+    coluna = input('Digite a coluna ')
+    #orientacao = input('Digite a orientacao ')
+    if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
+        print('Esta posição não está válida!')
+    else:
+        frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+        quantidade -= 1
