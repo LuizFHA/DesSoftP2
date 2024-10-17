f7b8bf62f726fbff804f3b1d0a9cd5312c8dc357
LuizAssaf <felipe.h.assaf@gmail.com>
2024-10-17 13:11:36 -0300

    Correção do código

--- funcoes.py
+++ funcoes.py
@@ -4,12 +4,12 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     if tamanho > 1:
         if orientacao == "vertical":
             i = 1
-            while i <= tamanho:
+            while i < tamanho:
                 posição.append([linha+i, coluna])
                 i += 1
         if orientacao == "horizontal":
             i = 1
-            while i <= tamanho:
+            while i < tamanho:
                 posição.append([linha, coluna+i])
                 i += 1
     return posição
