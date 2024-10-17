bbde3b9196e2003f0e45bdbe61d4637cbcf3682d
LuizAssaf <felipe.h.assaf@gmail.com>
2024-10-17 14:11:44 -0300

    função afundados

--- funcoes.py
+++ funcoes.py
@@ -50,3 +50,15 @@ def posiciona_frota(infofrota):
 #frota = {'porta-aviões': [[[4, 0], [5, 0], [6, 0], [7, 0]]], 'navio-tanque': [[[1, 8], [2, 8], [3, 8]], [[0, 1], [0, 2], [0, 3]]], 'contratorpedeiro': [[[5, 4], [5, 5]], [[6, 5], [6, 6]], [[0, 5], [1, 5]]], 'submarino': [[[2, 4]], [[2, 6]], [[2, 3]], [[9, 8]]]}
 
 #print(posiciona_frota(frota))
+
+def afundados(infofrota, tabuleiro):
+    mortos = 0
+    for tipodenavio in infofrota:
+        for navio in infofrota[tipodenavio]:
+            vivo = False
+            for localnavio in navio:
+                if tabuleiro[localnavio[0]][localnavio[1]] == 1:
+                    vivo = True
+            if not vivo:
+                mortos += 1
+    return mortos
