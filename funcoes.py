638f01b205b15f2c139e60af4b0708b36776b3b8
LuizAssaf <felipe.h.assaf@gmail.com>
2024-10-20 13:21:04 -0300

    Loop de jogo

--- funcoes.py
+++ funcoes.py
@@ -79,3 +79,14 @@ def posicao_valida(infofrota, linha, coluna, orientacao, tamanho):
 #frota = {'porta-aviões': [[[2, 1], [3, 1], [4, 1], [5, 1]]]}
 #linha: 8, coluna: 8, orientacao: vertical, tamanho: 3
 #print(posicao_valida(frota, 8, 8, 'vertical', 3))
+
+def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
+    texto = ''
+    texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
+    texto += '_______________________________      _______________________________\n'
+
+    for linha in range(len(tabuleiro_jogador)):
+        jogador_info = '  '.join([str(item) for item in tabuleiro_jogador[linha]])
+        oponente_info = '  '.join([info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
+        texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
+    return texto
--- programa.py
+++ programa.py
@@ -72,4 +72,61 @@ while quantidade > 0:
         frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
         quantidade -= 1
 
-print(frota)
+#print(frota)
+
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+
+taboponente = posiciona_frota(frota_oponente)
+
+tabjogador = posiciona_frota(frota)
+
+jogando = True
+
+listajogadas = []
+
+while jogando:
+    print(monta_tabuleiros(tabjogador, taboponente))
+    ataque = False
+    while ataque == False:
+        valido = False
+        while valido == False:
+            ataquel = int(input("Qual linha você quer atacar? "))
+            if ataquel > 9 or ataquel < 0:
+                print('Coluna inválida!')
+            else:
+                valido = True
+        valido = False
+        while valido == False:
+            ataquec = int(input("Qual coluna você quer atacar? "))
+            if ataquec > 9 or ataquec < 0:
+                print('Coluna inválida!')
+            else:
+                valido = True
+        if [ataquel, ataquec] not in listajogadas:
+            listajogadas.append([ataquel, ataquec])
+            ataque = True
+        else:
+            print('A posição linha',ataquel,'e coluna',ataquec,'já foi informada anteriormente!')
+    taboponente = faz_jogada(taboponente,ataquel,ataquec)
+    if afundados(frota_oponente, taboponente) == 10:
+        print('Parabéns! Você derrubou todos os navios do seu oponente!')
+        jogando = False
